0a781f6c01d3c9177a5c502dc906c9fcce3ee839
odinakholiqov <bobojon.kholikov@gmail.com>
2023-08-03 22:26:43 +0500

    async crud

---
+++ src/tests/test_ping.py
@@ -0,0 +1,12 @@
+import pytest
+from starlette.testclient import TestClient
+
+from app.main import app
+
+client = TestClient(app)
+
+
+def test_ping(test_app):
+    response = test_app.get("/ping")
+    assert response.status_code == 200
+    assert response.json() == {"ping": "pong!"} 
